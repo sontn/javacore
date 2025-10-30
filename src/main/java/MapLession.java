1b104e03169fc40cb8d47d9a42e860e402669e67
Son Tran Ngoc <son.tranngoc@emeal.nttdata.com>
2025-10-30 11:53:49 +0700

    Working with interface

--- src/main/java/MapLession.java
+++ src/main/java/MapLession.java
@@ -1,5 +1,17 @@
+
+import lombok.extern.java.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class MapLession {
+    private static final Logger log = LoggerFactory.getLogger(MapLession.class);
+
     public static void main(String[] args) {
-        System.out.println("This is map lession");
+        Person son = new Person();
+        son.setName("Tran Ngoc Son");
+        son.setAge(45);
+        System.out.println("Hello");
+        log.info("This is a person {}", son);
     }
 }
---
+++ src/main/java/Person.java
@@ -0,0 +1,7 @@
+import lombok.Data;
+
+@Data
+public class Person {
+    String name;
+    int age;
+}
