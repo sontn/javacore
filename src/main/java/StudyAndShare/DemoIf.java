222856b70de164d17d99e088b670786ad8bbbfae
Son Tran Ngoc <son.tranngoc@emeal.nttdata.com>
2026-05-12 09:56:31 +0700

    Demo if

---
+++ src/main/java/StudyAndShare/DemoIf.java
@@ -0,0 +1,12 @@
+package StudyAndShare;
+
+public class DemoIf {
+    public static void main(String[] args) {
+        int a = 4;
+        if ( a >= 5) {
+            System.out.println("a lon hon hoac bang 5");
+        } else {
+            System.out.println("a nho hon 5");
+        }
+    }
+}
