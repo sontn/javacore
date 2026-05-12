053f6f896477b1683096220c7610d54ddb69778b
Son Tran Ngoc <son.tranngoc@emeal.nttdata.com>
2026-05-12 10:07:14 +0700

    While

---
+++ src/main/java/StudyAndShare/While.java
@@ -0,0 +1,11 @@
+package StudyAndShare;
+
+public class While {
+    public static void main(String[] args) {
+        char i = 0;
+        while ( i < 256) {
+            System.out.print(i);
+            ++i;
+        }
+    }
+}
