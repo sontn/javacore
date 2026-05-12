7f164f7edd671ae0b729a5761aa99bbc8770ebbd
Son Tran Ngoc <son.tranngoc@emeal.nttdata.com>
2026-05-12 10:11:49 +0700

    Switch

---
+++ src/main/java/StudyAndShare/Switch.java
@@ -0,0 +1,15 @@
+package StudyAndShare;
+
+public class Switch {
+    public static void main(String[] args) {
+        int day = 3;
+        switch (day) {
+            case 1: System.out.println("Hom nay la ngay thu 2"); break;
+            case 2:
+                System.out.println("Hom nay la ngay thu 3"); break;
+            default:
+                System.out.println("Khong co ngay nao ca"); break;
+        }
+        System.out.println("This is a test");
+    }
+}
