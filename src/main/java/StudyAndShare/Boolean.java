26014074375dc3fd216a389d484ab6555e0af6a0
Son Tran Ngoc <son.tranngoc@emeal.nttdata.com>
2026-05-12 10:04:51 +0700

    Boolean and For

--- src/main/java/StudyAndShare/Boolean.java
+++ src/main/java/StudyAndShare/Boolean.java
@@ -1,4 +1,11 @@
 package StudyAndShare;
 
 public class Boolean {
+
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println(a&&b);
+        System.out.println(a||b);
+    }
 }
---
+++ src/main/java/StudyAndShare/For.java
@@ -0,0 +1,9 @@
+package StudyAndShare;
+
+public class For {
+    public static void main(String[] args) {
+        for ( char i = 0; i < 256; i++) {
+            System.out.print(i +" ");
+        }
+    }
+}
