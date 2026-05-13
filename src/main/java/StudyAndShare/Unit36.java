a2f47fa693543274d2ef0bb9e4dd0c1549f2f74e
Son Tran Ngoc <son.tranngoc@emeal.nttdata.com>
2026-05-13 14:17:28 +0700

    Override

---
+++ src/main/java/StudyAndShare/Unit36.java
@@ -0,0 +1,21 @@
+package StudyAndShare;
+
+public class Unit36 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.sound();
+    }
+}
+
+class Animal {
+    void sound() {
+        System.out.println("Animal make a sound");
+    }
+}
+
+class Dog extends Animal {
+//    @Override
+//    void sound() {
+//        System.out.println("Dog make a sound");
+//    }
+}
