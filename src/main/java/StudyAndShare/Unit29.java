5c38b5a4c0793182b276580c0702519be98047e6
Son Tran Ngoc <son.tranngoc@emeal.nttdata.com>
2026-05-13 13:52:23 +0700

    Import

---
+++ src/main/java/StudyAndShare/Unit29.java
@@ -0,0 +1,14 @@
+package StudyAndShare;
+
+import java.util.Scanner;
+
+public class Unit29 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Hello " + name);
+    }
+}
