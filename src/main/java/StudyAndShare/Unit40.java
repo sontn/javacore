5d836beb2b57f4c77c894c004ff632138d50f2ba
Son Tran Ngoc <son.tranngoc@emeal.nttdata.com>
2026-05-13 14:39:00 +0700

    Constructor cha con

---
+++ src/main/java/StudyAndShare/Unit40.java
@@ -0,0 +1,19 @@
+package StudyAndShare;
+
+public class Unit40 {
+}
+
+class Cha{
+    public Cha(int a) {
+
+    }
+//    public Cha() {
+//
+//    }
+}
+
+class Con extends Cha {
+    public Con(int number) {
+        super(number);
+    }
+}
