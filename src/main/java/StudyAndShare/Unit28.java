38b5fd5a17358ad5a080fe67b8421e9ade0131b1
Son Tran Ngoc <son.tranngoc@emeal.nttdata.com>
2026-05-13 13:46:45 +0700

    Static

---
+++ src/main/java/StudyAndShare/Unit28.java
@@ -0,0 +1,13 @@
+package StudyAndShare;
+
+public class Unit28 {
+    public static void main(String[] args) {
+        System.out.println(Config.APP_NAME);
+//        Config.APP_NAME = "Dfadf";
+    }
+
+}
+
+class Config {
+    final static String APP_NAME = "MyApp";
+}
