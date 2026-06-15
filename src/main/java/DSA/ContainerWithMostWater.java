7d9fdaf16c6da074b3bf0265c7f56ceab64cc99a
Son Tran Ngoc <son.tranngoc@emeal.nttdata.com>
2026-06-15 10:53:05 +0700

    DSA two pointers

---
+++ src/main/java/DSA/ContainerWithMostWater.java
@@ -0,0 +1,24 @@
+package DSA;
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int h = Math.min(height[left], height[right]);
+            int width = right - left;
+            int area = h * width;
+
+            maxArea = Math.max(maxArea, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
