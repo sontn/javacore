61ee5b5879e2a61b8dd86ffb6eb047da6b6c55af
Son Tran Ngoc <son.tranngoc@emeal.nttdata.com>
2026-06-17 09:45:33 +0700

    Longest Substring without repeating characters

---
+++ src/main/java/DSA/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,29 @@
+package DSA;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int left = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            while(set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right -left +1);
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(s)); // Out
+
+    }
+}
