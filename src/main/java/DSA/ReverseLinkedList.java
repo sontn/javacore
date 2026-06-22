5dce54bd0e195bb14ebbd85e60369786ecf47276
Son Tran Ngoc <son.tranngoc@emeal.nttdata.com>
2026-06-22 11:02:18 +0700

    Reversed linked list

---
+++ src/main/java/DSA/ReverseLinkedList.java
@@ -0,0 +1,55 @@
+package DSA;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class ReverseLinkedList {
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+
+        return prev;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.val + " -> ");
+            curr = curr.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        // Tạo list: 1 -> 2 -> 3 -> 4
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+
+        System.out.println("Original: ");
+        printList(head);
+
+        System.out.println("Reversed: ");
+        printList(reverseList(head));
+    }
+}
