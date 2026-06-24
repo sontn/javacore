b32a24d5bc691878edcd9ed320e3dc74cfb3d227
Son Tran Ngoc <son.tranngoc@emeal.nttdata.com>
2026-06-24 10:52:43 +0700

    Max depth of tree

---
+++ src/main/java/DSA/MaximumDepthofBinaryTree.java
@@ -0,0 +1,12 @@
+package DSA;
+
+public class MaximumDepthofBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
