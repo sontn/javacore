3c0ae337fafc30fec9d319300b31f7b503a3244c
Son Tran Ngoc <son.tranngoc@emeal.nttdata.com>
2026-06-24 10:07:08 +0700

    Invert tree

---
+++ src/main/java/DSA/InvertBinaryTree.java
@@ -0,0 +1,26 @@
+package DSA;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
