4e853478e0b3e87e7d127bd53a97776ed7e6ef42
Son Tran Ngoc <son.tranngoc@emeal.nttdata.com>
2026-06-25 09:52:28 +0700

    Lowest Common Ancestor

---
+++ src/main/java/DSA/LowestCommonAncestorInBST.java
@@ -0,0 +1,15 @@
+package DSA;
+
+public class LowestCommonAncestorInBST {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+}
