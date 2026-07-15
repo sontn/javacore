c25a36eead224bb940c7248f20802aa647464e4e
Son Tran Ngoc <son.tranngoc@emeal.nttdata.com>
2026-07-15 10:08:14 +0700

    DSA PrefixTree

---
+++ src/main/java/DSA/PrefixTree.java
@@ -0,0 +1,67 @@
+package DSA;
+
+public class PrefixTree {
+    private static class TrieNode {
+        TrieNode[] children;
+        boolean isEnd;
+
+        TrieNode() {
+            children = new TrieNode[26];
+            isEnd = false;
+        }
+    }
+
+    private TrieNode root;
+
+    public PrefixTree() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode current = root;
+
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+
+            if (current.children[index] == null) {
+                current.children[index] = new TrieNode();
+            }
+
+            current = current.children[index];
+        }
+
+        current.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode current = root;
+
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+
+            if (current.children[index] == null) {
+                return false;
+            }
+
+            current = current.children[index];
+        }
+
+        return current.isEnd;
+    }
+
+    public boolean startsWith(String  prefix) {
+        TrieNode current = root;
+
+        for (char c : prefix.toCharArray()) {
+            int index = c - 'a';
+
+            if (current.children[index] ==null) {
+                return false;
+            }
+
+            current = current.children[index];
+        }
+
+        return true;
+    }
+}
