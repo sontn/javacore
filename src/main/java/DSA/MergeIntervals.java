3d6825116230ec7291c772b6a9461ad574cf99bb
Son Tran Ngoc <son.tranngoc@emeal.nttdata.com>
2026-07-24 09:15:43 +0700

    Merge intervals

---
+++ src/main/java/DSA/MergeIntervals.java
@@ -0,0 +1,25 @@
+package DSA;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeIntervals {
+    public int[][] merge(int[][] intervals) {
+        if (intervals == null || intervals.length <= 1) return intervals;
+
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> merged = new ArrayList<>();
+
+        for (int[] interval : intervals) {
+            if (merged.isEmpty() || merged.get(merged.size() - 1)[1] < interval[0]) {
+                merged.add(interval);
+            } else {
+                merged.get(merged.size() -1)[1] = Math.max(merged.get(merged.size() -1 )[1], interval[1]);
+            }
+        }
+
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
