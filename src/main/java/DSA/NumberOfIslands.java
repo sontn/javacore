406e6b9b8242978cb966d911a693bbf62a70cf7c
Son Tran Ngoc <son.tranngoc@emeal.nttdata.com>
2026-07-29 09:51:39 +0700

    Number of Islands

---
+++ src/main/java/DSA/NumberOfIslands.java
@@ -0,0 +1,36 @@
+package DSA;
+
+public class NumberOfIslands {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length ==0) return 0;
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int islands = 0;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c]  == '1') {
+                    islands++;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+
+        return islands;
+    }
+
+    private void dfs(char[][] grid, int r, int c) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        if (r < 0 || c < 0 || r >= rows|| c >= cols || grid[r][c] =='0') return;
+
+        grid[r][c] = '0';
+
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r,  c -1);
+        dfs(grid, r, c + 1);
+    }
+}
