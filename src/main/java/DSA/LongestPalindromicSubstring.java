40b616f8a48ab89aa6aa67e21cfecf43685d8c66
Son Tran Ngoc <son.tranngoc@emeal.nttdata.com>
2026-08-06 10:00:56 +0700

    Longest Palindromic Substring

---
+++ src/main/java/DSA/LongestPalindromicSubstring.java
@@ -0,0 +1,35 @@
+package DSA;
+
+public class LongestPalindromicSubstring {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 2) return s;
+
+        int start = 0;
+        int maxLen = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+            // palindrome le
+            int len1 = expand(s, i, i);
+
+            // palindrome chan
+            int len2 = expand(s, i, i + 1);
+
+            int len = Math.max(len1, len2);
+
+            if (len > maxLen) {
+                maxLen = len;
+                start = i - (len - 1) /2;
+            }
+        }
+
+        return s.substring(start, start + maxLen);
+    }
+
+    private int expand(String s, int left, int right) {
+        while (left >=0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
