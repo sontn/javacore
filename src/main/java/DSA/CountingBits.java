d1c0d7fcdfb57091c2a10dcc6a8c2f2bbb28b15c
Son Tran Ngoc <son.tranngoc@emeal.nttdata.com>
2026-08-19 10:28:33 +0700

    Counting bits

---
+++ src/main/java/DSA/CountingBits.java
@@ -0,0 +1,13 @@
+package DSA;
+
+public class CountingBits {
+    public int[] countBits(int n) {
+        int[] dp = new int[n+1];
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i >> 1] + ( i & 1);
+        }
+
+        return dp;
+    }
+}
