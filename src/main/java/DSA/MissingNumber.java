582fcf67b5cc4684a92ff77d556a5675c87cd749
Son Tran Ngoc <son.tranngoc@emeal.nttdata.com>
2026-08-24 17:21:52 +0700

    Missing Number

---
+++ src/main/java/DSA/MissingNumber.java
@@ -0,0 +1,14 @@
+package DSA;
+
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int xor = nums.length;
+
+        for (int i =0; i < nums.length; i++ ) {
+            xor ^=i;
+            xor ^=nums[i];
+        }
+
+        return xor;
+    }
+}
